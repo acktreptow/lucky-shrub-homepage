bab63f69b5e1b218dfb7dd03d2d7bcbc70ac4f59
acktreptow <acktreptow@gmail.com>
2023-08-09 09:55:45 +0100

    add JS functionality

---
+++ index.js
@@ -0,0 +1,13 @@
+const designButton = document.getElementsByClassName("nav-link")[1];
+const plantsButton = document.getElementsByClassName("nav-link")[2];
+const contactButton = document.getElementsByClassName("nav-link")[3];
+
+designButton.onclick = () => {
+  alert("Design page coming soon");
+};
+plantsButton.onclick = () => {
+  alert("Plants page coming soon");
+};
+contactButton.onclick = () => {
+  alert("Contact page coming soon");
+};
