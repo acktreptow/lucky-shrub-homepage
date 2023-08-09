7a31d2c59f81f1a8a033e0e5faaa0458c0847a23
acktreptow <acktreptow@gmail.com>
2023-08-09 12:30:20 +0100

    add alerts to all links

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const designButton = document.getElementsByClassName("nav-link")[1];
 const plantsButton = document.getElementsByClassName("nav-link")[2];
 const contactButton = document.getElementsByClassName("nav-link")[3];
+const indoorPlantsButton = document.getElementById("promotion-link");
+const portfolioLink = document.getElementsByTagName("a")[5];
 
 designButton.onclick = () => {
   alert("Design page coming soon");
@@ -12,4 +14,12 @@ plantsButton.onclick = () => {
 
 contactButton.onclick = () => {
   alert("Contact page coming soon");
+};
+
+indoorPlantsButton.onclick = () => {
+  alert("Indoor plants are now sold out!");
+};
+
+portfolioLink.onclick = () => {
+  alert("Portfolio page coming soon!");
 };
